cc001fed8a27f9f9c015d4c8961722bf0008141b
soham22082003 <140507299+soham22082003@users.noreply.github.com>
2023-08-17 16:37:53 +0530

    Add files via upload

---
+++ hr q.7.sql
@@ -0,0 +1,14 @@
+create database hr_2;
+use hr_2 ;
+
+create table employes_1
+( 
+  hr_name varchar(30) primary key,
+  hr_namestartdate date 
+  );
+  
+  select * from employes_1 ;
+  
+  
+  
+  
